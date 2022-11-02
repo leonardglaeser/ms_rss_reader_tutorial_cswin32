2a58330e5a4d628cc4f94a6276867d42628ebe53
Leonard Gläser <dev@leonardglaeser.de>
2022-11-02 23:32:31 +0100

    init git

---
+++ ms_rss_reader_tutorial_cswin32/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Windows.Win32;
+using Windows.Win32.UI.WindowsAndMessaging;
+using Windows.Win32.Foundation;
+using Windows.Web.Syndication;
+
+
+
+
+var uri = new Uri("https://blogs.windows.com/feed");
+var client = new SyndicationClient();
+var feed = await client.RetrieveFeedAsync(uri);
+
+//Just to keep it similar to my bad rust impl
+List<string> titles = new List<string>();
+foreach (var item in feed.Items)
+{
+    var heading = item.Title.Text;
+    Console.WriteLine(heading);
+    titles.Add(heading);
+}
+
+var feedtext = string.Join("\n", titles);
+feedtext += "\0";
+
+
+//not necessary, done in the generated files!
+// unsafe
+// {
+//     fixed (char* lptext = feedtext)
+//     {
+//         PInvoke.MessageBox(default(HWND), lptext, "RSS_News", MESSAGEBOX_STYLE.MB_OK);
+//     }
+// }
+
+var lptext = feedtext;
+
+PInvoke.MessageBox(default(HWND), lptext, "RSS_News", MESSAGEBOX_STYLE.MB_OK);
